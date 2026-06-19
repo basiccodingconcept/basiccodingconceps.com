9f1e036208eb5e53bb63d331c62e4c927dd8e417
Basic Coding Concepts Deployer <deployer@basiccodingconcepts.io>
2026-06-19 18:35:18 +0500

    feat: add Google Analytics tracking tags to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
+import Script from "next/script";
 import { ThemeProvider } from "@/context/ThemeContext";
 import "./globals.css";
 
@@ -56,6 +57,19 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Analytics Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DHPEMCXZ1B"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DHPEMCXZ1B');
+          `}
+        </Script>
         <ThemeProvider>
           {children}
         </ThemeProvider>
